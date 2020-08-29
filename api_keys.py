0c7e49c9a78373685e3eaf4f6cb19184440504ab
paulhan49 <you@example.com>
2020-08-29 18:25:34 -0400

    master

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "13a9a713411f9501e4381ab155fb6b2b"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC5y1VSt62PrkiidoXRGV0DwodvDlrE9ak"
